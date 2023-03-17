1983323d5e9c137caaad726925cd5ab42f80a3b8
Pavan Kumar <pavanrnri1818@gmail.com>
2023-03-17 17:39:23 +0530

    first commit

---
+++ src/PAGES/HomePage/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from '../../COMPONENTS/Navbar/Navbar'
+
+export const Home = () => {
+  return (
+    <div>
+        <Navbar/>
+       
+        Home</div>
+  )
+}
